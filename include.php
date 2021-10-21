a15442cd425c94f2acc411138c68407851b7d975
Geoffrey Launay <glaunay@sio.local>
2021-10-21 17:02:42 +0200

    exos_de_merde

--- include.php
+++ include.php
@@ -1,7 +1,18 @@
 <?php
 include "header.php";
 ?>
-Contenu de la page
+<table border='1'>
+    <tbody>
+        <tr>
+            <td>1-1</td>
+            <td>1-2</td>
+        </tr>
+        <tr>
+            <td>2-1</td>
+            <td>2-2</td>
+        </tr>
+    </tbody>
+</table>
 <?php
 include "footer.php";
 ?>
